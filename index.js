ff560228531025b453a308d1037c036c391005f0
Sean Narron <seannarron9@gmail.com>
2020-11-30 20:30:09 -0800

    Add files via upload
    Hey all, I am new to javascript and trying to learn more most used tricks. I would be happy to hear some useful tips!

---
+++ index.js
@@ -0,0 +1,27 @@
+ 
+function myFunction() {
+var elmnt = document.getElementById("intro");
+elmnt.scrollIntoView();
+};
+      
+
+        
+$(document).ready(function() {
+    $('#main-load').hide().delay(200).fadeIn(1500);
+});
+
+      
+
+        
+$(document).ready(function() {
+    $('#banner').hide().delay(1400).fadeIn(1500);
+});
+
+      
+
+        
+$(document).ready(function() {
+    $('#button').hide().delay(2100).fadeIn(1500);
+});
+
+     
